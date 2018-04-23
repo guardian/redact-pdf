0bfe0c528d50b2352c897b5122245e61ed049afa
davidfurey <david.furey@guardian.co.uk>
2018-04-23 18:58:09 +0100

    Disable allowed hosts filter

--- app/wiring/AppComponents.scala
+++ app/wiring/AppComponents.scala
@@ -4,13 +4,18 @@ import akka.actor.ActorSystem
 import play.api.routing.Router
 import play.api.BuiltInComponentsFromContext
 import controllers.{AssetsComponents, HomeController}
+import play.api.mvc.EssentialFilter
 import play.filters.HttpFiltersComponents
+import play.filters.hosts.AllowedHostsFilter
 
 trait AppComponents extends AssetsComponents
   with HttpFiltersComponents {
   self: BuiltInComponentsFromContext =>
 
   implicit val as: ActorSystem = actorSystem
+
+  override def httpFilters: Seq[EssentialFilter] =
+    super.httpFilters.filterNot(_.getClass == classOf[AllowedHostsFilter])
 
   lazy val assetController = new controllers.Assets(httpErrorHandler, assetsMetadata)
 
