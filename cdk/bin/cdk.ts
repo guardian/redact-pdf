175f74a8b99e0ba7d5c7dc596e657290fb82e773
akash1810 <akash1810@users.noreply.github.com>
2023-02-20 14:20:26 +0000

    chore: Apply prettier
    Run via `npm run format`, prettier ensures consistent code formatting.
    
    For example, it ensures quote marks are uniform.

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,9 @@
-import "source-map-support/register";
-import { App } from "aws-cdk-lib";
-import { CvRedactTool } from "../lib/cv-redact-tool";
+import 'source-map-support/register';
+import { App } from 'aws-cdk-lib';
+import { CvRedactTool } from '../lib/cv-redact-tool';
 
 const app = new App();
-new CvRedactTool(app, "CvRedactTool-PROD", { stack: "hiring-and-onboarding", stage: "PROD" });
+new CvRedactTool(app, 'CvRedactTool-PROD', {
+	stack: 'hiring-and-onboarding',
+	stage: 'PROD',
+});
